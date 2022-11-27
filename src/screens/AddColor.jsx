db68845ac94f5bcc65e2ab2880ace8d56d4ea365
Cornerstone <fortunecornerstone@gmail.com>
2022-11-27 17:58:36 +0100

    feat: Create layout for add-color page

--- src/screens/AddColor.jsx
+++ src/screens/AddColor.jsx
@@ -1,11 +1,41 @@
-import React from 'react'
+import React, { useState } from "react";
+import toast from "react-hot-toast";
+import { Link } from "react-router-dom";
 
 const AddColor = () => {
+  const [user, setUser] = useState({
+    colorName: "",
+    colorCode: "",
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    toast.success("clicked");
+  };
+
   return (
     <div>
-      
-    </div>
-  )
-}
+      <form onSubmit={handleSubmit}>
+        <label>
+          <span>Enter a Color</span>
+          <input
+            type="color"
+            onChange={(e) => setUser(user, ...e.target.value)}
+          />
+        </label>
+        <label>
+          <span>Select Color</span>
+          <input
+            type="color"
+            onChange={(e) => setUser(user, ...e.target.value)}
+          />
+        </label>
+        <button>Add Color</button>
+      </form>
 
-export default AddColor
+      <Link to="/">Return home</Link>
+    </div>
+  );
+};
+
+export default AddColor;
