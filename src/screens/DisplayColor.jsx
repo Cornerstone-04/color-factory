727dd72596054b4d1522436319b024a7100b5193
Cornerstone <fortunecornerstone@gmail.com>
2022-11-28 11:43:06 +0100

    feat: style color display page

--- src/screens/DisplayColor.jsx
+++ src/screens/DisplayColor.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React from "react";
+import { Link, useParams } from "react-router-dom";
 
 const DisplayColor = () => {
+  const { colorName, colorCode } = useParams();
   return (
-    <div>
-      
+    <div
+      style={{ backgroundColor: "#" + colorCode }}
+      className="h-screen flex justify-center items-center flex-col"
+    >
+      <div className="flex flex-col gap-[10px] justify-center items-center p-[20px] bg-[#16161d] text-white">
+        <h1 className="text-xl ">
+          This is <span className="font-bold capitalize"> {colorName}</span>{" "}
+          with hex <span className="font-bold"> #{colorCode}</span>
+        </h1>
+        <Link to="/" className="text-cyan-500 hover:text-cyan-300">
+          Home
+        </Link>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default DisplayColor
+export default DisplayColor;
